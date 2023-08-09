25fe0407cee59cd1e48535ac9f98dd0d1acc1dcd
Dan Jenkins <danj@tuta.io>
2023-08-09 22:36:45 +0200

    [new] Added the ability to write project data to disk

---
+++ src/main/java/com/stata/io/IOManager.java
@@ -0,0 +1,66 @@
+package com.stata.io;
+
+import com.stata.project.Project;
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import static java.util.Map.entry;
+
+/**
+ * The IOManager class. This static class is responsible for reading and
+ * writing project data from the disk. Project data files take the form of a
+ * zip archive with the following structure:
+ * 
+ * Zip archive
+ *   - Metadata
+ * 
+ *  @author Dan Jenkins
+ */
+public class IOManager
+{
+    /**
+     * The save function. This saves a project state to the disk so that it can
+     * be exported or loaded again elsewhere.
+     * 
+     * @param filename The filename to save the rpoject as
+     * @param project The project state to save
+     * 
+     * @throws IOException
+     */
+    public static void save(String filename, Project project) throws IOException
+    {
+        // Create the path to write the file to
+        Path path = Path.of(filename);
+
+        // Create an output stream for writing a file
+        ZipOutputStream output = new ZipOutputStream(new BufferedOutputStream(Files.newOutputStream(path)));
+
+        // Create the file contents
+        Map<String, String> contents = Map.ofEntries
+        (
+            entry("metadata", project.getMetadata().getJSONString().toString())
+        );
+
+        // And iterate through the contents of the zip and write them
+        for (var entry : contents.entrySet())
+        {
+            // Add the entry
+            output.putNextEntry(new ZipEntry(entry.getKey()));
+
+            // Write the entry
+            output.write(entry.getValue().getBytes());
+
+            // And close the entry
+            output.closeEntry();            
+        }
+
+        // Finally, close the output stream
+        output.close();
+    }
+}
