8bf26f5e9a089d753ee1c2a6701a69065bf551e0
Dan Jenkins <danj@tuta.io>
2023-08-19 23:03:00 +0200

    [new] Added gllobal application ui updates

--- src/main/java/com/stata/ui/UI.java
+++ src/main/java/com/stata/ui/UI.java
@@ -1,9 +1,11 @@
 package com.stata.ui;
 
 import com.stata.Stata;
+import com.stata.project.Project;
 import com.stata.ui.scenes.MainScene;
 
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -17,6 +19,9 @@ public class UI extends Application
 {
     /** The parent Stata instance. */
     private final Stata stata;
+
+    /** The current stage in the window. */
+    private Stage stage;
 
     /** The root note of the interface. */
     private final BorderPane root;
@@ -44,20 +49,71 @@ public class UI extends Application
      * The start function. This is called whenever the JavaFX application
      * starts up.
      * 
-     * @param primary The primary stage
+     * @param stage The primary stage
      */
-    public void start(Stage primary)
+    public void start(Stage stage)
     {
-        // Set the window title
-        primary.setTitle(this.stata.getRuntime().getRuntimeValue("title", String.class));
+        // Store the stage
+        this.stage = stage;
+
+        // Make sure that all child components are updated
+        this.update();
 
         // Set the default scene
-        primary.setScene(this.scene);
+        this.stage.setScene(this.scene);
 
         // Update the scene's stage
-        this.scene.setStage(primary);
+        this.scene.setStage(this.stage);
 
         // And show the interface
-        primary.show();
+        this.stage.show();
+    }
+
+    /**
+     * The update function. This is used to trigger UI updates whenever the
+     * project state changes.
+     */
+    public void update()
+    {
+        // Get the updated project
+        Project project = Stata.getInstance().getProject();
+
+        // Update the title and icon of the window
+        this.stage.setTitle(this.createTitle(project));
+        this.stage.getIcons().add(new Image("/resources/stata.png"));
+
+        // Update the main scene
+        this.scene.update(this, project);
+    }
+
+    /**
+     * A simple function used to craft a title for the application. This takes
+     * the title of the application, appends the project information, and notes
+     * whether or not the file is saved.
+     * 
+     * @param project The project to use in the title.
+     * 
+     * @return The title of the application
+     */
+    public String createTitle(Project project)
+    {
+        // Create the title string
+        String title = this.stata.getRuntime().getRuntimeValue("title", String.class);
+
+        // Add the project information
+        if (project != null)
+        {
+            // Add the project title
+            title += " - " + project.getMetadata().getName();
+
+            // And add an asterisk if the file isn't saved
+            if (project.isModified())
+            {
+                title += "*";
+            }
+        }
+
+        // And return the string
+        return title;
     }
 }
