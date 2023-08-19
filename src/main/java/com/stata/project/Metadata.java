60cb5c99482b1b5eba94c11a2119c114db8105bd
Dan Jenkins <danj@tuta.io>
2023-08-19 23:01:03 +0200

    [new] Added project name to metadata

--- src/main/java/com/stata/project/Metadata.java
+++ src/main/java/com/stata/project/Metadata.java
@@ -10,16 +10,57 @@ import org.json.JSONObject;
  */
 public class Metadata
 {
+    /** The parent project for this meta data. */
+    private Project project;
+
     /** The date that this project was created. */
     private long date_create;
 
+    /** The name of the project. */
+    private String name;
+
     /**
      * The main constructor. This creates a new metadata object with the default values.
+     * 
+     * @param project The project that this metadata belongs to
      */
-    public Metadata()
+    public Metadata(Project project)
     {
+        // Store the parent project
+        this.project = project;
+
         // Set the creation data for this project
         this.date_create = System.currentTimeMillis();
+
+        // Set the name of the project
+        this.name = "New project";
+    }
+
+    /**
+     * The function used to return the name of the stored project.
+     * 
+     * @return The name of the project
+     */
+    public String getName()
+    {
+        return this.name;
+    }
+
+    /**
+     * A function used to set the name of the stored project.
+     * 
+     * @param name The name of the project
+     */
+    public void setName(String name)
+    {
+        // Note that the project is updated
+        if (!this.name.equals(name))
+        {
+            this.project.modify();
+        }
+
+        // And store the new name
+        this.name = name;
     }
 
     /**
@@ -34,6 +75,7 @@ public class Metadata
 
         // Store the parameters
         object.put("date_create", this.date_create);
+        object.put("project_name", this.name);
 
         // And return the object
         return object;
@@ -49,5 +91,6 @@ public class Metadata
     {
         // Extract the relevant variables
         this.date_create = object.getLong("date_create");
+        this.name = object.getString("project_name");
     }
 }
